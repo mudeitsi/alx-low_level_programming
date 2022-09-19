15cadea149a15b27f3c14a33084e7150098c2a70
mudeitsi <eserenash@gmail.com>
2022-09-19 10:10:13 -0700

    Task 0 Battery st

---
+++ 0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value to 98
+ *
+ * @n: pointer integer
+ *
+ * Description: takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * Return: value
+ */
+void reset_to_98(int *n)
+{
+		*n = 98;
+}
