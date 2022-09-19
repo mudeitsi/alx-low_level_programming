4c38520f5276e201e9b492f47f91e6a122f6beaf
mudeitsi <eserenash@gmail.com>
2022-09-19 12:34:53 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* _strlen - returns the length of a string
+*
+* @s: string
+* Return: returns length as integer
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
