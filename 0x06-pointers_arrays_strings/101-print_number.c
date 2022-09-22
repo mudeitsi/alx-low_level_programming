336541f1711924cf99bf986d3243f8dd9eaf011c
mudeitsi <eserenash@gmail.com>
2022-09-22 07:44:30 -0700

    corrected task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,45 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer
+ * print_number - prints number
+ * @n:integer to convert to character
  *
- * @n: the integer to print
- *
- * Return: nothing
  */
 
 void print_number(int n)
 {
+	unsigned int abs;
+	int mult = 1;
+	unsigned int abSCount;
+	int i;
+	int c = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
 	if (n < 0)
 	{
 		_putchar('-');
+		n += 1;
 		n *= -1;
+		n++;
 	}
+	abs = n;
+	abSCount = n;
 
-	if (n / 10)
-		print_number(n / 10);
+	while (abSCount > 0)
+	{
+		abSCount /= 10;
+		c++;
+	}
+	for (i = 0; i < c - 1; i++)
+		mult *= 10;
 
-	_putchar((n % 10) + '0');
+	for (i = 0; i < c; i++)
+	{
+		_putchar((abs / mult) + '0');
+		abs = abs % mult;
+		mult /= 10;
+	}
 }
