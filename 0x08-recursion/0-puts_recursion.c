415239c0b4637d395c3af48419c475536074b0e7
mudeitsi <eserenash@gmail.com>
2022-09-28 07:03:06 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string followed by a new line.
- * @s: the string to be printed
- *
- *
- * Return: void
+ * _puts_recursion - prints a string
+ * @s: the pointer input
+ * 
+ * Return: the string printed
  */
 
 void _puts_recursion(char *s)
@@ -13,10 +12,9 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
-	else
-	{
-		_putchar(s[0]);
-		_puts_recursion(s + 1);
-	}
+	_putchar(*s);
+	_puts_recursion(++s);
 }
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,24 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
+ifndef MAIN.H
 
-include <stdio.h>
+#define MAIN.H
 
-/**
-* void prototypes()
-* int prototypes()
-*/
-
-int _putchar(char c);
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
-int factorial(int n);
 int _pow_recursion(int x, int y);
+int factorial(int n);
+int square_root(int i, int j);
 int _sqrt_recursion(int n);
+int prime_found(int i, int j);
 int is_prime_number(int n);
-int _sqrt(int n, int x);
-int is_prime(int n, int start);
-void check(char *s, int start, int end, int *flag);
-int wildcmp(char *s1, char *s2);
+int is_palindrome(char *s);
 
-#endif /*MAIN_H*/
+#endif
