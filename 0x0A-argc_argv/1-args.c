df963a45ed934886ecff8bb1aa6fa6bb8a2c8b3e
mudeitsi <eserenash@gmail.com>
2022-09-30 03:27:48 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function prints all arguments received
+ * @argc: number of arguments passed
+ * Return: Always 0
+ * @argv: arguments passed here
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
