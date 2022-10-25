33245ccaf9f31a55b06be6196cb75ca4a56597b8
mudeitsi <eserenash@gmail.com>
2022-10-25 10:14:06 -0700

    task 4 correction

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
  * free_list - frees a list
- * @head: head of the linked list
- *
+ * @head: head var
  */
 
 void free_list(list_t *head)
 {
-	if (head != NULL)
+	list_t *temporary;
+
+	while (head != NULL)
 	{
+		temporary = head->next;
 		free(head->str);
-		free_list(head->next);
+		free(head);
+		head = temporary;
 	}
-	free(head);
 }
