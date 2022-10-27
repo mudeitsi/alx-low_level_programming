0cb444c981e704210b92d09cdab3725728a86566
mudeitsi <eserenash@gmail.com>
2022-10-27 08:42:03 -0700

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to the beginning of the list
+ *
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		free(head);
+		head = tmp->next;
+	}
+}
