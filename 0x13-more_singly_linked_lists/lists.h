10e2adfc0fb3b11f168b3986b0dbf48acfbe1854
mudeitsi <eserenash@gmail.com>
2022-10-27 10:18:53 -0700

    made additions to lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,26 +1,7 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
 #include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-int _putchar(char c);
-/**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for ALX project
- */
-
-typedef struct list_s
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
 
 /**
  * struct listint_s - singly linked list
@@ -28,7 +9,7 @@ typedef struct list_s
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for ALX project
+ * for alx project
  */
 
 typedef struct listint_s
@@ -36,11 +17,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -52,4 +29,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-#endif
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
+
+#endif /* _LISTS_H_ */
